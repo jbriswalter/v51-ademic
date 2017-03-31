636addb7f0344ed6dd3a3bab96f3372d91809ffd
bureau <bureau@bureau>
2017-03-31 18:18:06 +0200

    [MAJ] init

---
+++ WikiStatus/lang/english/common.php
@@ -0,0 +1,42 @@
+<?php
+/*##################################################
+ *                               WikiStatusModuleMiniMenu.class.php
+ *                            -------------------
+ *   begin                : January 30, 2017
+ *   copyright            : (C) 2016 Julien BRISWALTER
+ *   email                : j1.seth@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ * This program is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+
+ ####################################################
+#                                                          English                                                                        #
+ ####################################################
+
+$lang['menu_title'] = 'Articles created or updated';
+$lang['statut_legend'] = 'Only documents whose status has been declared (predefined or customized) are displayed';
+$lang['title_date_and_time'] = 'Date and time';
+$lang['title_author'] = 'Author';
+$lang['status_1'] = 'Article OK';
+$lang['status_2'] = 'Incomplete';
+$lang['status_3'] = 'Pending';
+$lang['status_4'] = 'Redo';
+$lang['status_5'] = 'Obsolete';
+?>
